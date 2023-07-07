4b0c46a217b1c644a2a169ecb76479135d838b62
Jovan <jovanpavlovic2018@gmail.com>
2023-07-07 12:44:53 +0200

    Added code for repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word,num) {
+    let str = '';
+    if(Math.sign(num) === -1){
+        return 'ERROR';
+    }
+    for(let i = 1;i <= num;i++){
+        str = str + word;
+    }
+    return str;
 };
 
 // Do not edit below this line
