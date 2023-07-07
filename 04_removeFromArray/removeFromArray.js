8574e7d600b71cbd4859f9fd79f205e4db4745ed
Jovan <jovanpavlovic2018@gmail.com>
2023-07-07 15:40:31 +0200

    Added code for removing elements from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (myArr, ...args) {
+    
+    return myArr.filter(x => !args.includes(x));
+    
+    //let index = myArr.indexOf(myArr, ...args);
+    //myArr.splice(index,1);
+    //return myArr
 };
 
 // Do not edit below this line
