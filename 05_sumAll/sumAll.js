4f51bf8dbca4b1bb7f767e6c6f9de3d15794839d
Jovan <jovanpavlovic2018@gmail.com>
2023-07-09 10:37:06 +0200

    added code for sum between two numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+    let sum = 0;
+    if(Math.sign(min) === -1 || Math.sign(max) === -1 || !Number.isInteger(min) || !Number.isInteger(max)){
+        return 'ERROR';
+    }
+    else if (max < min) {
+        for (let i = max; i <= min; i++) {
+            sum = sum + i;
+        }
+    }
+    else {
+        for (let i = min; i <= max; i++) {
+            sum = sum + i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
