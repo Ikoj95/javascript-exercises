e4aeac3353ba08cfd7996da3162b47dab03471c5
Jovan <jovanpavlovic2018@gmail.com>
2023-08-14 11:53:41 +0200

    code for calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function ([...args]) {
+  if (args.length === 0) {
+    return 0;
+  }
+  return args.reduce((a, b) => a + b);
 };
 
-const multiply = function() {
-
+const multiply = function (...args) {
+  return args.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  let result = 1;
+  while (a) {
+    result = result * a;
+    a--;
+  }
+  return result;
 };
 
 // Do not edit below this line
