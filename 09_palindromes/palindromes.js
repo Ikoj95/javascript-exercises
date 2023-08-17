2d61ba82e5a175527bf296dc0aa0bc1a7eb7ffde
Jovan <jovanpavlovic2018@gmail.com>
2023-08-17 13:47:21 +0200

    code for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    const placeholder = word.toLowerCase().replaceAll(/[\s.,?!]/g, "");
+    const reverse = placeholder.split("").reverse().join("");
+
+    if (placeholder === reverse) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
