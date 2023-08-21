2a4987846b37bb16c33ccc385736996dbb0bcd1f
Jovan <jovanpavlovic2018@gmail.com>
2023-08-21 13:12:52 +0200

    code for oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (e) {
+    const date = new Date();
+    e.forEach(object => {
+        if (!object['yearOfDeath']) {
+            object['yearOfDeath'] = date.getFullYear();
+        }
+    })
+    const res = e.sort((a, b) => {
+        const old = a.yearOfDeath - a.yearOfBirth;
+        const young = b.yearOfDeath - b.yearOfBirth;
+        if (old > young) {
+            return -1;
+        }
+        else {
+            return 1;
+        }
+    });
+    return res[0];
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
