556d1edccc13b46091ad3a84efd25d4d4b7cfa8c
Jovan <jovanpavlovic2018@gmail.com>
2023-08-21 11:25:18 +0200

    code for fibbonaci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
 
+    if (Math.sign(n) == -1) {
+        return 'OOPS';
+    }
+
+    let fiboArr = [1, 1];
+    for (let i = 0; i < n; i++) {
+        let sum = 0;
+        sum = fiboArr[i] + fiboArr[i + 1];
+        fiboArr.push(sum);
+    }
+    return fiboArr[n-1];
 };
 
 // Do not edit below this line
